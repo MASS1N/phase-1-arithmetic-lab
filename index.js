60ba9c42704488375efba895f8817d010fbe271c
MUSTAFA MAZIN AHMED <mustafa.hamedi.2001@gmail.com>
2024-02-15 15:32:21 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 64;
+const num4 = 60;
+const mod = num3 % num4;
+const max = Math.max(1, 5, 10, 15, 20);
+
+console.log('multiply:', multiply);
+console.log('random:', random);
+console.log('mod:', mod);
+console.log('max:', max);
+
+
+ 
